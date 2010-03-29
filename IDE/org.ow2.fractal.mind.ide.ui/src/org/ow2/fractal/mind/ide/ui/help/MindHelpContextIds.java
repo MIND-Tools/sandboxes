7ab8c78244f8f767eefbc9fc1178fbd7418116cd
Raphael Proust <raphael.proust@logica.com>
2010-03-29 09:34:00 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/sandboxes@249 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- IDE/org.ow2.fractal.mind.ide.ui/src/org/ow2/fractal/mind/ide/ui/help/MindHelpContextIds.java
+++
@@ -1,9 +0,0 @@
-package org.ow2.fractal.mind.ide.ui.help;
-
-import org.ow2.fractal.mind.ide.ui.Activator;
-
-public class MindHelpContextIds {
-	public static final String PREFIX= Activator.PLUGIN_ID + '.';
-	public static final String BP_CHOOSE_EXISTING_FOLDER_TO_MAKE_SOURCE_FOLDER= PREFIX + "bp_choose_existing_folder_to_make_source_folder"; //$NON-NLS-1$
-	
-}
--- IDE/org.ow2.fractal.mind.ide.ui/src/org/ow2/fractal/mind/ide/ui/wizards/PageUdapteStatus.java
+++
@@ -1,9 +0,0 @@
-package org.ow2.fractal.mind.ide.ui.wizards;
-
-public interface PageUdapteStatus {
-
-	void updateErrorStatus(String msg);
-
-	void dialogChanged();
-
-}
