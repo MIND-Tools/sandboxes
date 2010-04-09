0023e32596f5731079de9f744427c4650aa9e463
Marc Poulhies <marc.poulhies@imag.fr>
2010-04-09 17:38:29 +0000

    Code generation seems ok (compiles fine). Scheduler is still added in the ADL and not injected.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/sandboxes@385 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- mindc-dataflow-bip/beam-frontend/src/main/java/org/ow2/mind/beam/annotation/BeamFifoAnnotationProcessor.java
+++ mindc-dataflow-bip/beam-frontend/src/main/java/org/ow2/mind/beam/annotation/BeamFifoAnnotationProcessor.java
@@ -37,6 +37,8 @@ import org.objectweb.fractal.adl.interfaces.Interface;
 import org.objectweb.fractal.adl.interfaces.InterfaceContainer;
 import org.objectweb.fractal.adl.types.TypeInterface;
 import org.objectweb.fractal.adl.util.FractalADLLogManager;
+import org.ow2.mind.CommonASTHelper;
+import org.ow2.mind.InputResourcesHelper;
 import org.ow2.mind.adl.annotation.ADLLoaderPhase;
 import org.ow2.mind.adl.annotation.AbstractADLLoaderAnnotationProcessor;
 import org.ow2.mind.adl.annotation.predefined.BeamFifo;
@@ -45,9 +47,19 @@ import org.ow2.mind.adl.ast.Binding;
 import org.ow2.mind.adl.ast.BindingContainer;
 import org.ow2.mind.adl.ast.Component;
 import org.ow2.mind.adl.ast.ComponentContainer;
+import org.ow2.mind.adl.ast.DefinitionReference;
+import org.ow2.mind.adl.ast.ImplementationContainer;
 import org.ow2.mind.adl.ast.MindInterface;
+import org.ow2.mind.adl.ast.Source;
+import org.ow2.mind.adl.idl.InterfaceDefinitionDecorationHelper;
 import org.ow2.mind.annotation.Annotation;
 import org.ow2.mind.beam.Constants;
+import org.ow2.mind.idl.ast.InterfaceDefinition;
+import org.ow2.mind.idl.ast.Method;
+import org.ow2.mind.idl.ast.Parameter;
+import org.ow2.mind.idl.ast.PrimitiveType;
+import org.ow2.mind.idl.ast.Type;
+import org.ow2.mind.idl.ast.TypeContainer;
 
 public class BeamFifoAnnotationProcessor
     extends
@@ -55,12 +67,162 @@ public class BeamFifoAnnotationProcessor
     implements
       Constants {
 
+  
+  /*
+   * Stolen from "InterfaceSignatureLoader"
+   */
+  protected void processItf(final Definition def, final TypeInterface itf,
+      final Definition container, final Map<Object, Object> context)
+      throws ADLException {
+    InterfaceDefinition itfDef;
+    try {
+      itfDef = itfSignatureResolverItf.resolve(itf, container, context);
+    } catch (final ADLException e) {
+      throw e;
+    }
 
+    InterfaceDefinitionDecorationHelper.setResolvedInterfaceDefinition(itf, itfDef);
+    InputResourcesHelper.addInputResources(def, InputResourcesHelper
+        .getInputResources(itfDef));
+  }
+  
   protected static Logger logger = FractalADLLogManager
   .getLogger("beam-binding-annot");
   
   static List<Binding> fifo_bindings = new ArrayList<Binding>();
 
+  static int buffer_uniq_id = 0;
+  
+  static PrimitiveType getPrimitiveType(Node n){
+    assert(n instanceof TypeContainer);
+    Type n_type = ((TypeContainer)n).getType();
+    assert(n_type instanceof PrimitiveType);
+    return (PrimitiveType) n_type;
+  }
+  
+  protected void addCCode(StringBuffer stringbuf, String code){
+    stringbuf.append(code+"\n");
+  }
+  
+  protected Source createFifoBufferImplementation(String name, String ifacename, 
+      String signature, Integer size, Definition comp_def,
+      MindInterface iface, final Map<Object, Object> context) throws ADLException{
+
+    
+    InterfaceDefinition ifacedef = itfSignatureResolverItf.resolve(iface,
+        comp_def, context);
+    
+    String returnType = null;
+    String paramType = null;
+    
+    StringBuffer generated_code = new StringBuffer();
+    
+    for (Method m: ifacedef.getMethods()){
+      PrimitiveType mpt = getPrimitiveType(m);
+
+      if (m.getName().equals("get")){
+        returnType = mpt.getName();
+        assert(m.getParameters().length == 0);
+        
+      } else if (m.getName().equals("put")) {
+        assert(m.getParameters().length == 1);
+        
+        for (Parameter p : m.getParameters()){
+          assert(p instanceof TypeContainer);
+          PrimitiveType ppt = getPrimitiveType(p);
+          paramType = ppt.getName();
+        }
+      } else {
+        assert(false);
+      }
+    }
+    assert(returnType.equals(paramType));
+
+    /*
+     * We checked that only get/put methods are needed and that their types are
+     * correct. We can generate code for both method
+     */
+    /*
+     * Common part
+     */
+    addCCode(generated_code, "#include \"beam/template/buffer.h\"");
+    addCCode(generated_code, "/* the fifo type */");
+    addCCode(generated_code, "FIFO_TYPE(" + returnType + ", " + size + ", "+ buffer_uniq_id +")");
+    addCCode(generated_code, "/*  Private Data  */");
+    addCCode(generated_code, "TYPE_NAME(" + returnType + ", "+ buffer_uniq_id + ") myFifo;");
+
+    addCCode(generated_code, "// use macro to create fifo's methods");
+    addCCode(generated_code, "FIFO_SIZE(" + returnType + ", " + size + ", "+ buffer_uniq_id + ")");
+    addCCode(generated_code, "FIFO_INIT(" + returnType + ", "+ buffer_uniq_id + ")");
+    addCCode(generated_code, "FIFO_PUSH(" + returnType + ", " + size + ", "+ buffer_uniq_id + ")");
+    addCCode(generated_code, "FIFO_POP(" + returnType + ", " + size + ", "+ buffer_uniq_id + ")");
+    addCCode(generated_code, "FIFO_TOP(" + returnType + ", "+ buffer_uniq_id + ")");
+    addCCode(generated_code, "FIFO_REQUEUE(" + returnType + ", "+ buffer_uniq_id + ")");
+
+    /*
+     * for GET
+     */
+    addCCode(generated_code, "/* Server method for GET*/");
+    addCCode(generated_code, returnType + " METH(buffer,get) (void) {");
+    addCCode(generated_code, "return FIFO_POP_N(" + returnType + ", " + buffer_uniq_id + ")(&myFifo);");
+    addCCode(generated_code, "}");
+
+    /*
+     * for PUT
+     */
+    addCCode(generated_code, "/* Server method for PUT */");
+    addCCode(generated_code, "void METH(buffer, put)(" + returnType + " x) {");
+    addCCode(generated_code, "FIFO_PUSH_N(" + returnType + ", " + buffer_uniq_id + ")(x, &myFifo);");
+    addCCode(generated_code, "}");
+    
+    final Source src = CommonASTHelper.newNode(nodeFactoryItf, "source", Source.class);
+    src.setCCode(generated_code.toString());
+    return src;
+  }
+  protected Component createFifoBufferComponent(String name, String ifacename, 
+      String signature, Integer size, final Map<Object, Object> context, Definition container_def) 
+      throws ADLException{
+
+    buffer_uniq_id++;
+
+    Definition new_bufdef = ASTHelper.newPrimitiveDefinitionNode(nodeFactoryItf, "beam.generated.buffer" + name, 
+        (DefinitionReference[]) null);
+    MindInterface iface = ASTHelper.newServerInterfaceNode(nodeFactoryItf,
+        ifacename, signature);
+    
+    processItf(container_def, iface, container_def, context);
+    
+    assert(new_bufdef instanceof InterfaceContainer);
+    assert(new_bufdef instanceof ImplementationContainer);
+    
+    ((InterfaceContainer)new_bufdef).addInterface(iface);
+    Source implem = createFifoBufferImplementation(name, ifacename, signature, size,
+        new_bufdef, iface, context);
+    
+    DefinitionReference dr = ASTHelper.newDefinitionReference(nodeFactoryItf, 
+        "beam.generated.buffer" + name);
+    ASTHelper.setResolvedDefinition(dr, new_bufdef);
+    
+    ((ImplementationContainer)new_bufdef).addSource(implem);
+    
+    Component buffer_comp = ASTHelper.newComponent(
+        nodeFactoryItf, 
+        name, 
+        dr);
+    
+    
+    return buffer_comp;
+  }
+  
+  protected String getBufferMangledName(Binding b){
+    String comp_from = b.getFromComponent();
+    String comp_to = b.getToComponent();
+    String iface_from = b.getFromInterface();
+    String iface_to = b.getToInterface();
+    
+    String name = comp_from + "_" + iface_from + "__" + comp_to + "_" + iface_to;
+    return name;
+  }
   // ---------------------------------------------------------------------------
   // Implementation of the ADLLoaderAnnotationProcessor interface
   // ---------------------------------------------------------------------------
@@ -74,18 +236,25 @@ public class BeamFifoAnnotationProcessor
     BeamFifo bt = (BeamFifo) annotation;
     Binding b = (Binding) node;
 
-    String newbuffer_name = "buffer";
+    String newbuffer_name = getBufferMangledName(b);
     
     if (phase == ADLLoaderPhase.AFTER_CHECKING) {
-      Component buffer_comp = ASTHelper.newComponent(
-          nodeFactoryItf, 
-          newbuffer_name, 
-          "helloworld.TestBuffer");
+      
+      Component from_comp = ASTHelper.getComponent(definition, b.getFromComponent());
+      assert(from_comp != null);
+      Definition from_def = ASTHelper.getResolvedComponentDefinition(from_comp, loaderItf, context);
+      Interface iface =ASTHelper.getInterface(from_def, b.getFromInterface());
+      assert(iface instanceof MindInterface);
+      String signature = ((MindInterface)iface).getSignature();
+      
+      Component buffer_comp = createFifoBufferComponent(newbuffer_name, "buffer", 
+          signature, bt.fifosize, context, definition);
       
       assert(buffer_comp != null);
-      Definition buffer_def = defRefResolverItf.resolve(buffer_comp.getDefinitionReference(), definition, context);
+      Definition buffer_def = defRefResolverItf.resolve(buffer_comp.getDefinitionReference(), 
+          definition, context);
       assert(buffer_def!=null);
-        ASTHelper.setResolvedComponentDefinition(buffer_comp, buffer_def);
+      ASTHelper.setResolvedComponentDefinition(buffer_comp, buffer_def);
 
       assert(buffer_def != null);
       assert(buffer_def instanceof InterfaceContainer);
@@ -100,7 +269,7 @@ public class BeamFifoAnnotationProcessor
       assert(to_def != null);
       assert(to_def instanceof InterfaceContainer);
       for (Interface i : ((InterfaceContainer)to_def).getInterfaces()){
-        if (i.getName().equals("s")){
+        if (i.getName().equals(b.getToInterface())){
           assert(i instanceof MindInterface);
           MindInterface mi = (MindInterface)i;
           assert(mi.getRole().equals(TypeInterface.SERVER_ROLE));
@@ -114,7 +283,7 @@ public class BeamFifoAnnotationProcessor
       filter_buffer_b.setFromComponent(b.getToComponent());
       filter_buffer_b.setFromInterface(b.getToInterface());
       filter_buffer_b.setToComponent(newbuffer_name);
-      filter_buffer_b.setToInterface("s");
+      filter_buffer_b.setToInterface("buffer");
       
       assert(definition instanceof BindingContainer);
       ((BindingContainer) definition).addBinding(filter_buffer_b);
@@ -123,7 +292,7 @@ public class BeamFifoAnnotationProcessor
           filter_buffer_b.getToComponent() + "." + filter_buffer_b.getToInterface());
       
       b.setToComponent(newbuffer_name);
-      b.setToInterface("s");
+      b.setToInterface("buffer");
       logger.log(Level.INFO, "Retargeted binding to buffer.s");
 
     } else if (phase == ADLLoaderPhase.AFTER_PARSING) {
