8e7fde1899c32ad748383247b7f0d727fc1c3872
Marc Poulhies <marc.poulhies@imag.fr>
2010-04-30 07:51:33 +0000

    Merge commit 'trunk' into mindc-dataflow-bip
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/sandboxes@706 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- mindc-dataflow-bip/mindc/src/main/java/org/ow2/mind/Launcher.java
+++ mindc-dataflow-bip/mindc/src/main/java/org/ow2/mind/Launcher.java
@@ -198,6 +198,11 @@ public class Launcher extends AbstractLauncher {
                                                                       "no-bin",
                                                                       "Do not generate binary ADL/IDL ('.def', '.itfdef' and '.idtdef' files).");
 
+  protected final CmdFlag              extensionPointsListOpt     = new CmdFlag(
+                                                                      null,
+                                                                      "extension-points",
+                                                                      "Print the list of available extension points and exit.");
+
   protected boolean                    generateSrc;
   protected boolean                    compileDef;
 
@@ -238,6 +243,11 @@ public class Launcher extends AbstractLauncher {
     final STNodeFactoryImpl stNodeFactory = new STNodeFactoryImpl();
 
     final BasicPluginManager pluginManager = new BasicPluginManager();
+    final ClassLoader pluginClassLoader = BasicPluginManager
+        .getPluginClassLoader(compilerContext);
+    if (pluginClassLoader != null) {
+      pluginManager.setClassLoader(pluginClassLoader);
+    }
     pluginManager.nodeFactoryItf = stNodeFactory;
 
     try {
@@ -260,6 +270,18 @@ public class Launcher extends AbstractLauncher {
     // If help is asked, print it and exit.
     if (helpOpt.isPresent(cmdLine)) {
       printHelp(System.out);
+      System.exit(0);
+    }
+
+    // If the extension points list is asked, print it and exit.
+    if (extensionPointsListOpt.isPresent(cmdLine)) {
+      try {
+        printExtensionPoints(pluginManager, compilerContext, System.out);
+      } catch (final ADLException e) {
+        throw new CompilerInstantiationException(
+            "Cannot invoke command line option '"
+                + extensionPointsListOpt.longName + "'.", e, 101);
+      }
       System.exit(0);
     }
 
@@ -301,7 +323,6 @@ public class Launcher extends AbstractLauncher {
       compilerContext.put(disableDefaultBackendOpt.longName, true);
 
     checkADLMode = checkADLModeOpt.isPresent(cmdLine);
-
     generateSrc = generateDefSrcOpt.isPresent(cmdLine);
     compileDef = compileDefOpt.isPresent(cmdLine);
 
@@ -438,6 +459,17 @@ public class Launcher extends AbstractLauncher {
     } catch (final ADLException e) {
       throw new CompilerInstantiationException(
           "Cannot instantiate the compiler.", e, 101);
+    }
+  }
+
+  private void printExtensionPoints(final PluginManager pluginManager,
+      final Map<Object, Object> context, final PrintStream out)
+      throws ADLException {
+    final Collection<String> extensionPoints = pluginManager
+        .getExtensionPointNames(context);
+    System.out.println("Supported extension points are : ");
+    for (final String extensionPoint : extensionPoints) {
+      System.out.println("\t'" + extensionPoint + "'");
     }
   }
 
@@ -738,12 +770,14 @@ public class Launcher extends AbstractLauncher {
         includePathOpt, linkerCmdOpt, ldFlagsOpt, ldPathOpt, linkerScriptOpt,
         concurrentJobCmdOpt, printStackTraceOpt, checkADLModeOpt,
         disableDefaultBackendOpt, generateDefSrcOpt, compileDefOpt, forceOpt,
-        keepTempOpt, noBinASTOpt);
+        keepTempOpt, noBinASTOpt,
+        extensionPointsListOpt);
 
     for (final CmdOption option : CommandLineOptionExtensionHelper
         .getCommandOptions(pluginManagerItf, context)) {
       options.addOption(option);
     }
+
   }
 
   protected void invokeOptionHandlers(final PluginManager pluginManagerItf,
@@ -766,7 +800,8 @@ public class Launcher extends AbstractLauncher {
     ps.println("Usage: " + prgName + " [OPTIONS] (<definition>[:<execname>])+");
     ps.println("  where <definition> is the name of the component to"
         + " be compiled, ");
-    ps.println("  and <execname> is the name of the output file to be created.");
+    ps
+        .println("  and <execname> is the name of the output file to be created.");
   }
 
   protected void handleException(final InvalidCommandLineException e) {
