c9c1925cff7a108e8227f66695189e7e9080d219
Marc Poulhies <marc.poulhies@imag.fr>
2011-02-03 10:42:22 +0000

    protected version of dynamic fifo bug fix in dynfifo: was (bad) mix of LIFO & FIFO => incorrect ordering of element.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/sandboxes@1363 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

---
+++ mindc-dataflow-bip/example/beam-comp-library/beam/template/dynbuffer-protect.h
@@ -0,0 +1,84 @@
+#ifndef __BEAM_DYNBUFFER_TEMPLATE__H
+#define __BEAM_DYNBUFFER_TEMPLATE__H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define FIFO_HALT(str) do{printf("%s %s\n", __FUNCTION__, str);for(;;);}while(0)
+
+#define FIFO_INIT_N(ptype,id) _dynfifo_init_##ptype##id
+#define FIFO_INIT(ptype,id)				\
+  void FIFO_INIT_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
+     f->size = 0; \
+     f->first = NULL; \
+     f->last = NULL; \
+  }
+
+
+#define FIFO_SIZE_N(ptype,id) _dynfifo_size_##ptype##id
+#define FIFO_SIZE(ptype,usize,id)				\
+  int FIFO_SIZE_N(ptype,id) (TYPE_NAME(ptype,id) *f) {		\
+     return f->size;  \
+  }
+
+
+#define FIFO_PUSH_N(ptype,id) _dynpush_##ptype##id
+#define FIFO_PUSH(ptype,usize,id)				\
+  void FIFO_PUSH_N(ptype,id) (ptype x, TYPE_NAME(ptype,id) *f) {	\
+      ELT_TYPE_NAME(ptype,id) *new = malloc(sizeof(ELT_TYPE_NAME(ptype,id))); \
+      if (new == NULL) FIFO_HALT("Error allocating memory"); \
+      new->data = x; \
+      new->next = f->first; \
+      new->prev = NULL; \
+      if (f->size == 0){ \
+         f->last = new; \
+      } else { \
+         f->first->prev = new; \
+      } \
+      f->first = new; \
+      f->size++; \
+  }
+
+#define FIFO_POP_N(ptype,id) _dynpop_##ptype##id
+#define FIFO_POP(ptype,usize,id)			\
+  ptype FIFO_POP_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
+    ELT_TYPE_NAME(ptype,id) *elt; \
+     if (!f->size) FIFO_HALT("Underflow"); \
+     elt = f->last;		\
+     ptype res = f->last->data; \
+     if (f->last->prev != NULL) \
+        f->last->prev->next = NULL; \
+     f->last = f->last->prev; \
+     f->size--; \
+     if (f->size == 0){ \
+       f->first = NULL; \
+     } \
+     free(elt); \
+     return res; \
+  }
+
+#define FIFO_PEEK_N(ptype,id) _dynpeek_##ptype##id
+#define FIFO_PEEK(ptype,usize,id)				\
+  ptype FIFO_PEEK_N(ptype,id) (int i, TYPE_NAME(ptype,id) *f) {	\
+     int _i=i; \
+     ELT_TYPE_NAME(ptype,id) *elt; \
+     if (f->size<i) FIFO_HALT("Underflow"); \
+     elt = f->last;	\
+     while (_i){ \
+        if (elt->prev != NULL){			\
+           elt = elt->prev; \
+           _i--; \
+        } else		    \
+	  FIFO_HALT("Underflow");		\
+     } \
+     return elt->data; \
+  }
+
+#define FIFO_TOP_N(ptype,id) _dyntop_##ptype##id
+#define FIFO_TOP(ptype,id)			\
+  ptype FIFO_TOP_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
+     if (!f->size) FIFO_HALT("Underflow"); \
+     return f->last->data; \
+  }
+
+#endif /*  __BEAM_DYNBUFFER_TEMPLATE__H */
--- mindc-dataflow-bip/example/beam-comp-library/beam/template/dynbuffer.h
+++ mindc-dataflow-bip/example/beam-comp-library/beam/template/dynbuffer.h
@@ -1,75 +1,10 @@
-#ifndef __BEAM_DYNBUFFER_TEMPLATE__H
-#define __BEAM_DYNBUFFER_TEMPLATE__H
+#ifndef __BEAM_DYNBUFFER_MAIN__H /* should be useless, but never hurts */
+#define __BEAM_DYNBUFFER_MAIN__H
 
-#include <stdlib.h>
+#ifdef FIFO_DETECT_BAD_USE
+#include "dynbuffer-protect.h"
+#else
+#include "dynbuffer-unsafe.h"
+#endif
 
-#define FIFO_INIT_N(ptype,id) _dynfifo_init_##ptype##id
-#define FIFO_INIT(ptype,id)				\
-  void FIFO_INIT_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
-     f->size = 0; \
-     f->first = NULL; \
-     f->last = NULL; \
-  }
-
-
-#define FIFO_SIZE_N(ptype,id) _dynfifo_size_##ptype##id
-#define FIFO_SIZE(ptype,usize,id)				\
-  int FIFO_SIZE_N(ptype,id) (TYPE_NAME(ptype,id) *f) {		\
-     return f->size;  \
-  }
-
-
-#define FIFO_PUSH_N(ptype,id) _dynpush_##ptype##id
-#define FIFO_PUSH(ptype,usize,id)				\
-  void FIFO_PUSH_N(ptype,id) (ptype x, TYPE_NAME(ptype,id) *f) {	\
-      ELT_TYPE_NAME(ptype,id) *new = malloc(sizeof(ELT_TYPE_NAME(ptype,id)));		\
-      new->data = x; \
-      new->next = NULL; \
-      new->prev = f->last; \
-      if (f->size == 0){ \
-         f->first = new; \
-      } else { \
-         f->last->next = new; \
-      } \
-      f->last = new; \
-      f->size++; \
-  }
-
-#define FIFO_POP_N(ptype,id) _dynpop_##ptype##id
-#define FIFO_POP(ptype,usize,id)			\
-  ptype FIFO_POP_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
-     ELT_TYPE_NAME(ptype,id) *elt = f->last; \
-     ptype res = f->last->data; \
-     if (f->last->prev != NULL) \
-        f->last->prev->next = NULL; \
-     f->last = f->last->prev; \
-     f->size--; \
-     if (f->size == 0){ \
-       f->first = NULL; \
-     } \
-     free(elt); \
-     return res; \
-  }
-
-#define FIFO_PEEK_N(ptype,id) _dynpeek_##ptype##id
-#define FIFO_PEEK(ptype,usize,id)				\
-  ptype FIFO_PEEK_N(ptype,id) (int i, TYPE_NAME(ptype,id) *f) {	\
-     int _i=i; \
-     ELT_TYPE_NAME(ptype,id) *elt = f->last; \
-     while (_i){ \
-        if (elt->prev != NULL){			\
-           elt = elt->prev; \
-           _i--; \
-        } else		    \
-           for(;;); \
-     } \
-     return elt->data; \
-  }
-
-#define FIFO_TOP_N(ptype,id) _dyntop_##ptype##id
-#define FIFO_TOP(ptype,id)			\
-  ptype FIFO_TOP_N(ptype,id) (TYPE_NAME(ptype,id) *f) {	\
-     return f->last->data; \
-  }
-
-#endif /*  __BEAM_DYNBUFFER_TEMPLATE__H */
+#endif /*  __BEAM_DYNBUFFER_MAIN__H */
