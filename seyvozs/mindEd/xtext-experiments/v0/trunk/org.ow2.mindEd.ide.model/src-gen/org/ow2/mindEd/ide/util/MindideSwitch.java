e41a82627df2e935c8bd16ce4dcb9822ee59c0f9
Stephane Seyvoz <sseyvoz@orange-ftgroup.com>
2012-07-20 15:34:21 +0000

    Moving to latest Eclipse frameworks and SDKs
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/sandboxes@1582 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- seyvozs/mindEd/xtext-experiments/v0/trunk/org.ow2.mindEd.ide.model/src-gen/org/ow2/mindEd/ide/util/MindideSwitch.java
+++ seyvozs/mindEd/xtext-experiments/v0/trunk/org.ow2.mindEd.ide.model/src-gen/org/ow2/mindEd/ide/util/MindideSwitch.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.util.Switch;
 import org.ow2.mindEd.ide.model.*;
 
 /**
@@ -26,7 +28,7 @@ import org.ow2.mindEd.ide.model.*;
  * @see org.ow2.mindEd.ide.model.MindidePackage
  * @generated
  */
-public class MindideSwitch<T> {
+public class MindideSwitch<T> extends Switch<T> {
 	/**
 	 * The cached model package
 	 * <!-- begin-user-doc -->
@@ -48,14 +50,16 @@ public class MindideSwitch<T> {
 	}
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+	 * Checks whether this is a switch for the given package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
+	 * @parameter ePackage the package in question.
+	 * @return whether this is a switch for the given package.
 	 * @generated
 	 */
-	public T doSwitch(EObject theEObject) {
-		return doSwitch(theEObject.eClass(), theEObject);
+	@Override
+	protected boolean isSwitchFor(EPackage ePackage) {
+		return ePackage == modelPackage;
 	}
 
 	/**
@@ -65,26 +69,7 @@ public class MindideSwitch<T> {
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	protected T doSwitch(EClass theEClass, EObject theEObject) {
-		if (theEClass.eContainer() == modelPackage) {
-			return doSwitch(theEClass.getClassifierID(), theEObject);
-		}
-		else {
-			List<EClass> eSuperTypes = theEClass.getESuperTypes();
-			return
-				eSuperTypes.isEmpty() ?
-					defaultCase(theEObject) :
-					doSwitch(eSuperTypes.get(0), theEObject);
-		}
-	}
-
-	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+	@Override
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch (classifierID) {
 			case MindidePackage.MIND_REPO: {
@@ -438,6 +423,7 @@ public class MindideSwitch<T> {
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
+	@Override
 	public T defaultCase(EObject object) {
 		return null;
 	}
